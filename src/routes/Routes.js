3b99322ca0e9069cf632c817ae85c3a916031ee6
shivagunasehkaran <sivakumar.gunasehkaran@axiatadigitallabs.com>
2021-11-18 18:10:09 +0530

    Create routes file for tabs

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,10 @@
 // page names - public
 export const pageNamePublicStack = 'PublicStack';
+export const pageNamePrivateStack = 'PrivateStack';
+
 export const pageNameLogin = 'Login';
 export const pageNameRegister = 'Register';
 export const pageNameDashboard = 'Dashboard';
+export const pageNameSettings = 'Settings';
+export const pageNameChats = 'Chats';
+export const pageNameProfile = 'Profile';
