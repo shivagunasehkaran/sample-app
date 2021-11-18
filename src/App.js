755ea4c14d89165e1f892f336060fca882cb537b
shivagunasehkaran <sivakumar.gunasehkaran@axiatadigitallabs.com>
2021-11-18 14:05:46 +0530

    Folder structure and Sample UI changes

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>{'Hello world!!'}</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
