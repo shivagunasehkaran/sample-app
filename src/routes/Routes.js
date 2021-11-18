34e52ad2eeb6a381bcea01d860fd8867c09519d6
shivagunasehkaran <sivakumar.gunasehkaran@axiatadigitallabs.com>
2021-11-18 17:23:44 +0530

    Create routes file

---
+++ src/routes/Routes.js
@@ -0,0 +1,5 @@
+// page names - public
+export const pageNamePublicStack = 'PublicStack';
+export const pageNameLogin = 'Login';
+export const pageNameRegister = 'Register';
+export const pageNameDashboard = 'Dashboard';
