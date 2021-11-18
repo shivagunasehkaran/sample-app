b93fab7cf51ecf44fb97a054d314b43d5417f85c
shivagunasehkaran <sivakumar.gunasehkaran@axiatadigitallabs.com>
2021-11-18 17:18:16 +0530

    Create container to set up navigation

--- src/App.js
+++ src/App.js
@@ -7,20 +7,10 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-  View,
-} from 'react-native';
+import PrimaryContainer from './containers';
 
 const App = () => {
-  return (
-    <SafeAreaView style={{flex:1}}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>{'Hello world!!'}</Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <PrimaryContainer />;
 };
 
 export default App;
---
+++ src/containers/PrimaryContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+// base stack
+const PrimaryContainer = props => {
+  return <Text>{'Primary container'}</Text>;
+};
+
+export default PrimaryContainer;
