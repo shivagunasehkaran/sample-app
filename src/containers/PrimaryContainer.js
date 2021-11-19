91aa4887351afdaf621d3d2cab94df60d30d2ecb
shivagunasehkaran <sivakumar.gunasehkaran@axiatadigitallabs.com>
2021-11-19 16:25:48 +0530

    Handle stacks based on redux value (isAuthenticatedUser)

--- src/containers/PrimaryContainer.js
+++ src/containers/PrimaryContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {useSelector} from 'react-redux';
 // route imports
 import * as NAV_ROUTES from '../routes/Routes';
 import * as NAVIGATION from '../routes/Navigator';
@@ -7,14 +8,28 @@ import * as NAVIGATION from '../routes/Navigator';
 const Stack = createStackNavigator();
 
 const PrimaryContainer = props => {
+  // fetch redux values
+  const isAuthenticatedUser = useSelector(
+    state => state.reducer.loginDetails.isAuthenticatedUser,
+  );
+  
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        options={{headerShown: false}}
-        key={NAV_ROUTES.pageNamePublicStack}
-        name={NAV_ROUTES.pageNamePublicStack}
-        component={NAVIGATION.PublicRoutes}
-      />
+      {!isAuthenticatedUser ? (
+        <Stack.Screen
+          options={{headerShown: false}}
+          key={NAV_ROUTES.pageNamePublicStack}
+          name={NAV_ROUTES.pageNamePublicStack}
+          component={NAVIGATION.PublicRoutes}
+        />
+      ) : (
+        <Stack.Screen
+          options={{headerShown: false}}
+          key={NAV_ROUTES.pageNamePrivateStack}
+          name={NAV_ROUTES.pageNamePrivateStack}
+          component={NAVIGATION.PrivateRoutes}
+        />
+      )}
     </Stack.Navigator>
   );
 };
